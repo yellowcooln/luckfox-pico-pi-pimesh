f3cf91f09bf621d3eac6a8f4261fe873af9ebefe
yellowcooln <12516003+yellowcooln@users.noreply.github.com>
2026-04-23 16:10:46 -0400

    Ensure /dev/tty exists in image

--- board/luckfox/pico-pi-pimesh/post-fakeroot.sh
+++ board/luckfox/pico-pi-pimesh/post-fakeroot.sh
@@ -6,3 +6,10 @@ TARGET_DIR="$1"
 mkdir -p "${TARGET_DIR}/var/empty"
 chown 0:0 "${TARGET_DIR}/var/empty"
 chmod 0755 "${TARGET_DIR}/var/empty"
+
+mkdir -p "${TARGET_DIR}/dev"
+if [ ! -e "${TARGET_DIR}/dev/tty" ]; then
+  mknod -m 0666 "${TARGET_DIR}/dev/tty" c 5 0
+fi
+chown 0:5 "${TARGET_DIR}/dev/tty"
+chmod 0666 "${TARGET_DIR}/dev/tty"
