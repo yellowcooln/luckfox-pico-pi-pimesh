72cbeed30128c350719685dcebfd93e116bf0395
yellowcooln <12516003+yellowcooln@users.noreply.github.com>
2026-04-28 15:00:07 -0400

    Fix shipped shell prompt

--- board/luckfox/pico-pi/rootfs-overlay/etc/profile.d/prompt.sh
+++ board/luckfox/pico-pi/rootfs-overlay/etc/profile.d/prompt.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
+# Use BusyBox ash fancy prompt escapes so SSH/login shells look like Debian:
+# root@luckfox:~#  and  user@luckfox:~$
 if [ -n "${PS1:-}" ]; then
-  if [ "$(id -u)" -eq 0 ]; then
-    export PS1='[\u \W]# '
-  else
-    export PS1='[\u \W]$ '
-  fi
+  PS1='\u@\h:\w\$ '
+  export PS1
 fi
